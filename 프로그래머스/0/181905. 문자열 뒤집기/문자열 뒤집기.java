64b3a2fabcc2a9522dc23cc8a7ead1fcb8c04d84
김관진 <cozyschooll@gmail.com>
2024-01-29 14:20:00 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        StringBuilder answer = new StringBuilder(my_string);
+        StringBuilder sb = new StringBuilder(my_string.substring(s, e+1));
+        sb.reverse();
+        answer.replace(s, e+1, sb.toString());
+        return answer.toString();
+    }
+}
